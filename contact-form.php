766caf7ccdd4e8cbadc7751400f962779445a285
Dvij Barot <dvijbarot17@gmail.com>
2020-11-17 13:48:21 +0530

    Send Message Working

--- contact-form.php
+++ contact-form.php
@@ -6,11 +6,13 @@
 
     $email_from = 'dvijbarot17@gmail.com';
     $email_subject = 'New Inquiry';
-    $email_body = "USer Name: $name. \n". "User Email: $email. \n". "User Message: $message. \n";
+    $email_body = "USer Name: $name. \n".
+                     "User Email: $email. \n".
+                         "User Message: $message. \n";
 
     $to = "dvijbarot17@gmail.com";
     $headers = "From : $email_from \r\n";
     $headers .= "Reply-To : $email \r\n";
     mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html")
+    header("Location: index.html");
 ?>
